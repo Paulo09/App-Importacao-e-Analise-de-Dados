3aca688a0812145b223875d3a0b1f685e993fcfb
Paulo Castro <paullocasttro@hotmail.com>
2024-11-12 16:10:45 -0300

    Script Criação Tabela - Postgres
    Script Criação Tabela - Postgres

---
+++ resultados_analises.sql
@@ -0,0 +1,14 @@
+﻿-- Table: resultados_analises
+
+-- DROP TABLE resultados_analises;
+
+CREATE TABLE resultados_analises
+(
+  nome_analise "char"[],
+  data_analise date[]
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE resultados_analises
+  OWNER TO postgres;
