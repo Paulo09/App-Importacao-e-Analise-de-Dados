6716dabe752b15d6aa78526e351d5d8cabfa7811
Paulo Castro <paullocasttro@hotmail.com>
2024-11-12 16:07:15 -0300

    Script Criar Database

---
+++ analises_db.sql
@@ -0,0 +1,12 @@
+﻿-- Database: analises_db
+
+-- DROP DATABASE analises_db;
+
+CREATE DATABASE analises_db
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
